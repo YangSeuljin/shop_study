e903b2cf44b32b24d4b45c31fe65850ac96ed256
yang <gluestuck@naver.com>
2022-08-21 18:11:38 +0900

    - 스프링 시큐리티 적용 - 회원가입페이지 구현

---
+++ src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,36 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+/*
+@RequiredArgesConstructor 어노테이션은 final @NonNull이 붙은 필드에 생성샂를 생성해줌.
+빈에 생성자가 1개이고 생성자의 파라미터 타입이 빈으로 등록이 가능하다면 @Autowired 어노테이션 없이 의존성 주입이 가능함.
+ */
+@RequiredArgsConstructor
+/*
+ * 비즈니스 로직을 담당하는 서비스 계층 클래스에
+ *@Transactional 어노테이션을 선언. 로직을 처리하다가 에러가 발생하면, 변경된 데이터를 로직을 수행하기 이전 상태로 콜백 시켜줌.*/
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+
+        //이미 가입된 회원의 경우 IllegalStateException 예외를 발생시킴.
+        if (findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
---
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -0,0 +1,62 @@
+package com.shop.service;
+
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+//테스트 클래스에 @Transactional 어노테이션을 선언할 경우, 테스트 실행 후 롤백 처리가 됨. 이를 통해 같은 메소도를 반복적으로 테스트할 수 있음.
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember() {
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@email.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 광진구 자양동");
+        memberFormDto.setPassword("as1234ad");
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest() {
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getAddress(), savedMember.getAddress());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getRole(), savedMember.getRole());
+    }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest() {
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        //Junt의 Assertions 클래스의 assertThrows 메서드를 이요하면 예외 처리 테스트가 가능함. 첫 번째 파라미터에는 발생할 예외 타입을 넣어줌.
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);
+        });
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
+}
