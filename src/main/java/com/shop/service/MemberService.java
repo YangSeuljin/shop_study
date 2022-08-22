36f39ec7a385b8806d8b4c298e76d1a24104048b
yang <gluestuck@naver.com>
2022-08-23 00:06:29 +0900

    - 스프링 부트 버전 변경 - 레이아웃 css 변경 - 스프링 시큐리티 설정 변경

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -3,6 +3,10 @@ package com.shop.service;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,7 +21,7 @@ import javax.transaction.Transactional;
  * 비즈니스 로직을 담당하는 서비스 계층 클래스에
  *@Transactional 어노테이션을 선언. 로직을 처리하다가 에러가 발생하면, 변경된 데이터를 로직을 수행하기 이전 상태로 콜백 시켜줌.*/
 @Transactional
-public class MemberService {
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member) {
@@ -32,5 +36,20 @@ public class MemberService {
         if (findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
