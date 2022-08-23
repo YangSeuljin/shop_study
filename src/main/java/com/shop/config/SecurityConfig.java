ba0c3f9306665790494bf743d290eb7bf4aaf96e
yang <gluestuck@naver.com>
2022-08-23 22:59:14 +0900

    - 페이지 권한 설정하기

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +32,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) //로그아웃 URL을 설정
                 .logoutSuccessUrl("/");
 
+        //시큐리티 처리에 HttpServiceRequest를 이용한다는 것을 의미
+        //permitAll()을 통해 모든 사용자가 인증없이 해당 경로에 접근할 수 있도록 설정.
+        http.authorizeRequests().mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                // /admin으로 시작하는 경로는 해당 계정이 ADMIN Role일 경우에만 접근이 가능하도록 설정.
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                // 설정해준 경로를 제외한 나머지 경로들은 모두 인증을 요구하도록 설정.
+                .anyRequest().authenticated();
+
+        //인증되지 않은 사용자가 리소스에 접근하였을 때 수행되는 핸들러를 등록함.
+        http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+    }
+
+    //static 디렉터리의 하위 파일은 인증을 무시하도록 설정.
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     @Bean
