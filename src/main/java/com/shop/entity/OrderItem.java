39b629529670ca3c2f10a26ef893f3a6e9785cdc
yang <gluestuck@naver.com>
2022-09-24 18:51:24 +0900

    - 주문 이력 조회하기

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
-public class OrderItem extends BaseEntity{
+public class OrderItem extends BaseEntity {
     @Id
     @GeneratedValue
     @Column(name = "order_item_id")
@@ -30,4 +29,28 @@ public class OrderItem extends BaseEntity{
     //private LocalDateTime regTime;
 
     //private LocalDateTime updateTime;
+
+    public static OrderItem createOrderItem(Item item, int count) {
+        OrderItem orderItem = new OrderItem();
+
+        //주문할 상품과 주문 수량을 세팅
+        orderItem.setItem(item);
+        orderItem.setCount(count);
+
+        //현재 시간을 기준으로 상품 가격을 주문 가격으로 세팅. 상품 가격은 시간에 따라서 달라질 수 있음.
+        orderItem.setOrderPrice(item.getPrice());
+
+        //주문 수량만큼 상품의 재고 수량 감소
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    /**
+     * 주문 가격과 주문 수량을 곱해서 해당 상품을 주문한 총 가격을 계산하는 메서드
+     *
+     * @return
+     */
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 }
